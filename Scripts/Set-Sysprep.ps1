ad95f9412ee14fe9691761c92734113deaabf3e6
Darren Lowe <darren.j.lowe@gmail.com>
2020-04-20 14:55:02 -0700

    updated sysprep

--- Scripts/Set-Sysprep.ps1
+++ Scripts/Set-Sysprep.ps1
@@ -1,5 +1,5 @@
 # Copy files to correct locations in prior to sysprep
 Write-Host "Copying UK auto unattend file" -ForegroundColor Green
-New-Item -Path 'C:\Windows\Preping\Unattend' -ItemType 'Directory' -Force
-Copy-Item -Path 'a:\Autounattend.xml' -Destination 'C:\Windows\Preping\Unattend\unattend.xml'
+New-Item -Path 'C:\Windows\Panther\Unattend' -ItemType 'Directory' -Force
+Copy-Item -Path 'a:\Autounattend.xml' -Destination 'C:\Windows\Panther\Unattend\unattend.xml'
 
